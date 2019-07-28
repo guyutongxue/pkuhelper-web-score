411b3caa92ec73f4644f9c8634080a37941a070f
xmcp <xiaoyuanan2000@sina.com>
2019-07-28 12:11:27 +0800

    update style

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,13 @@ export default function Footer(props) {
     return (
         <div className="footer">
             <p>学期GPA和总GPA为公式计算所得，请以学校官方结果为准！</p>
-            <p>基于 GPLv3 协议在 <a href="https://github.com/pkuhelper-web/score" target="_blank">GitHub</a> 开源</p>
+            <p>
+                基于&nbsp;
+                <a href="https://www.gnu.org/licenses/gpl-3.0.zh-cn.html" target="_blank">GPLv3</a>
+                &nbsp;协议在&nbsp;
+                <a href="https://github.com/pkuhelper-web/score" target="_blank">GitHub</a>
+                &nbsp;开源
+            </p>
         </div>
     )
 }
