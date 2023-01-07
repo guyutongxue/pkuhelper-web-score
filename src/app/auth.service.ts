c6d5825331807ed24c95ea2a56cbffdb46b988b8
Guyutongxue <guyutongxue@163.com>
2023-01-07 12:40:02 +0800

    remove pkuhelper

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,39 +1,43 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
 import { DataService } from './data.service';
 import { shownScoreHelper } from './shown_score_helper';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  loginMethod: "pkuhelper" | "iaaa" = localStorage['LOGIN_METHOD'] ?? "pkuhelper";
-  token = localStorage['TOKEN'] ?? "";
-  username = localStorage['USERNAME'] ?? "";
-  password = "";
+  username: string = localStorage['USERNAME'] ?? '';
+  password: string = '';
 
   loading = false;
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    const params = new URLSearchParams(this.document.location.search);
+    if (params.has("username")) {
+      this.username = params.get("username")!;
+    }
+    if (params.has("password")) {
+      this.password = params.get("password")!;
+    }
+  }
 
   async load() {
     this.loading = true;
     try {
       this.dataService.clear();
-      if (this.loginMethod === "pkuhelper") {
-        if (localStorage['LOGIN_METHOD'] !== "pkuhelper" || this.token !== localStorage['TOKEN']) {
-          shownScoreHelper.clear();
-        }
-        await this.dataService.loadFromUrl(`https://pkuhelper.pku.edu.cn/api_xmcp/isop/scores?user_token=${encodeURIComponent(this.token)}`);
-        localStorage['TOKEN'] = this.token;
-      } else {
-        if (localStorage['LOGIN_METHOD'] !== "iaaa" || this.username !== localStorage['USERNAME']) {
-          shownScoreHelper.clear();
-        }
-        await this.dataService.loadFromUrl(`/api/iaaa?username=${encodeURIComponent(this.username)}&password=${encodeURIComponent(this.password)}`);
-        localStorage['USERNAME'] = this.username;
+      if (this.username !== localStorage['USERNAME']) {
+        shownScoreHelper.clear();
       }
-      localStorage['LOGIN_METHOD'] = this.loginMethod;
+      await this.dataService.loadFromUrl(
+        `/api/iaaa?username=${encodeURIComponent(
+          this.username
+        )}&password=${encodeURIComponent(this.password)}`
+      );
+      localStorage['USERNAME'] = this.username;
     } finally {
       this.loading = false;
     }
